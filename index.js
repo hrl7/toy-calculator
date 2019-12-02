4106163a86459c45a5005056c18b909dcee55871
zigen <zigen@horol.org>
2019-12-03 00:53:37 +0900

    impl order

--- index.js
+++ index.js
@@ -1,12 +1,40 @@
-const calc = (str) => {
-  let i = 0, c = "", tokens = [];
+const NodeType = {
+  program: "program",
+  number: "number",
+  binaryOperator: "binaryOperator"
+};
+class Node {
+  constructor(type, left, right, op) {
+    this.type = type;
+    switch (type) {
+      case NodeType.number:
+        this.value = left;
+        break;
+      case NodeType.binaryOperator:
+        this.left = left;
+        this.right = right;
+        this.op = op;
+        break;
+      case NodeType.program:
+        this.expr = null;
+        break;
+      default:
+        throw new Error(`unexpected node type ${type}`);
+    }
+  }
+}
+
+const calc = str => {
+  let i = 0,
+    c = "",
+    tokens = [];
   str = str.replace(/\s+/g, "");
-  while(str[i] !== undefined) {
-    switch(str[i]) {
-      case '+':
-      case '-':
-      case 'x':
-      case '/':
+  while (str[i] !== undefined) {
+    switch (str[i]) {
+      case "+":
+      case "-":
+      case "x":
+      case "/":
         tokens.push(Number(c));
         tokens.push(str[i]);
         c = "";
@@ -20,35 +48,80 @@ const calc = (str) => {
   tokens.push(Number(c));
   console.log("tokens: ", tokens);
 
-  i = 1;
-  let acc = tokens[0];
-  while(tokens[i] !== undefined) {
-    switch(tokens[i]) {
-      case '+':
-        acc += tokens[i + 1];
+  i = 0;
+  const parse = () => {
+    const consume = token => {
+      console.log(
+        `consume ${token}, ${tokens[i]} at ${i} : ${token === tokens[i]}`
+      );
+      if (token === tokens[i]) {
         i++;
-        break;
-      case '-':
-        acc -= tokens[i + 1];
-        i++;
-        break;
-      case 'x':
-        acc *= tokens[i + 1];
-        i++;
-        break;
-      case '/':
-        acc /= tokens[i + 1];
-        i++;
-        break;
-      default:
-        console.log(tokens[i]);
-        throw new Error("unexpected operator");
-        break;
+        return true;
+      }
+      return false;
+    };
+    const expr = () => {
+      return add();
+    };
+    const mul = () => {
+      console.log(`mul: ${tokens[i]}, ${i}`);
+      let node = num();
+      while (true) {
+        if (consume("x")) {
+          node = new Node(NodeType.binaryOperator, node, num(), "x");
+        } else if (consume("/")) {
+          node = new Node(NodeType.binaryOperator, node, num(), "/");
+        } else {
+          return node;
+        }
+      }
+    };
+
+    const add = () => {
+      console.log(`add: ${tokens[i]}, ${i}`);
+      let node = mul();
+      while (true) {
+        console.log(i);
+        if (consume("+")) {
+          node = new Node(NodeType.binaryOperator, node, mul(), "+");
+        } else if (consume("-")) {
+          node = new Node(NodeType.binaryOperator, node, mul(), "-");
+        } else {
+          return node;
+        }
+      }
+    };
+    const num = () => {
+      const token = tokens[i++];
+      assertType(token, NodeType.number);
+      return new Node(NodeType.number, token);
+    };
+
+    return expr();
+  };
+  const ast = parse();
+  console.log(str, ast);
+  const evaluate = node => {
+    switch (node.type) {
+      case NodeType.program:
+        return evaluate(node.expr);
+      case NodeType.number:
+        return node.value;
+      case NodeType.binaryOperator: {
+        switch (node.op) {
+          case "+":
+            return evaluate(node.left) + evaluate(node.right);
+          case "x":
+            return evaluate(node.left) * evaluate(node.right);
+          case "/":
+            return evaluate(node.left) / evaluate(node.right);
+          case "-":
+            return evaluate(node.left) - evaluate(node.right);
+        }
+      }
     }
+  };
+  return evaluate(ast);
+};
 
-    i++;
-  }
-  return acc;
-}
-
-module.exports = calc
+module.exports = calc;
--- index.test.js
+++ index.test.js
@@ -1,5 +1,11 @@
 const calc = require("./index");
 
+test("number", () => {
+  expect(calc("3")).toBe(3);
+  expect(calc("12")).toBe(12);
+  expect(calc("9999")).toBe(9999);
+});
+
 test("plus", () => {
   expect(calc("1+2")).toBe(3);
   expect(calc("2+2 + 5")).toBe(9);
@@ -9,4 +15,6 @@ test("multiply", () => {
   expect(calc("1x2")).toBe(2);
   expect(calc("2x2 + 5")).toBe(9);
   expect(calc("1x2x3x4")).toBe(24);
+  expect(calc("2+ 2 x 5")).toBe(12);
+  expect(calc("1 + 2 x 3 + 4 x 5")).toBe(27);
 });
